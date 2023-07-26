31a618dd6f8e6480d5a31051cc0e54c7db8b677e
Vivian Zhu <vivian.yijia.zhu@gmail.com>
2023-07-26 06:19:11 -0700

    Test4

--- increasingsubsequence.py
+++ increasingsubsequence.py
@@ -6,17 +6,19 @@ class Solution:
 
             if(len(arr)!=0):
                 count=1
+                newcount=0
                 for i in range(1,len(arr)):
                     if(arr[i]>arr[i-1]):
                         count+=1
                     else:
                         count=1
+                        newcount+=1
                 print(arr)
                 print(count)
             else:
                 count=0
             
-            return count
+            return count+newcount
             #TODO: Write code below to return an int with the solution to the prompt.
 
 def main():
