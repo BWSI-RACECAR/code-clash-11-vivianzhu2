933f713848682cc5f6acd29b0b4b4866818ca2cd
Vivian Zhu <vivian.yijia.zhu@gmail.com>
2023-07-26 06:10:29 -0700

    Test1

---
+++ increasingsubsequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def find_longest_increasing_subsequence(self, arr):
+            #type arr: list of int
+            #return type: int
+            #arr=arr.sort()
+            count=1
+            
+            for i in range(1,arr):
+                if(arr[i]>arr[i+1]):
+                    count+=1
+                else:
+                    count=0
+            
+            #TODO: Write code below to return an int with the solution to the prompt.
+            pass
+
+def main():
+    array = input().split(" ")
+    for x in range (0, len(array)):
+        array[x] = int(array[x])
+
+    tc1 = Solution()
+    ans = tc1.find_longest_increasing_subsequence(array)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
